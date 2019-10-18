d7488ac26fa25d92e6fd0d0ef62fc99c390afce9
Alin Floare <alin.floare@plentydevelopment.com>
2019-10-18 11:16:06 +0300

    Chag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,78 +1,40 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, PreloadAllModules } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-/*
- * Platform and Environment providers/directives/pipes
- */
-import { environment } from 'environments/environment';
-import { ROUTES } from './app.routes';
-// App is our top level component
+import { environment } from '@env/environment';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { ShellModule } from './shell/shell.module';
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
 import { AppComponent } from './app.component';
-import { APP_RESOLVER_PROVIDERS } from './app.resolver';
-import { AppState, InternalStateType } from './app.service';
-import { HomeComponent } from './home';
-import { AboutComponent } from './about';
-import { NoContentComponent } from './no-content';
-import { XLargeDirective } from './home/x-large';
-import { DevModuleModule } from './+dev-module';
+import { AppRoutingModule } from './app-routing.module';
 
-import '../styles/styles.scss';
-import '../styles/headings.css';
-
-// Application wide providers
-const APP_PROVIDERS = [
-  ...APP_RESOLVER_PROVIDERS,
-  AppState
-];
-
-interface StoreType {
-  state: InternalStateType;
-  restoreInputValues: () => void;
-  disposeOldHosts: () => void;
-}
-
-/**
- * `AppModule` is the main entry point into Angular2's bootstraping process
- */
 @NgModule({
-  bootstrap: [ AppComponent ],
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    NoContentComponent,
-    XLargeDirective
-  ],
-  /**
-   * Import Angular's modules.
-   */
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
+    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production }),
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(ROUTES, {
-      useHash: Boolean(history.pushState) === false,
-      preloadingStrategy: PreloadAllModules
-    }),
-
-    /**
-     * This section will import the `DevModuleModule` only in certain build types.
-     * When the module is not imported it will get tree shaked.
-     * This is a simple example, a big app should probably implement some logic
-     */
-    ...environment.showDevModule ? [ DevModuleModule ] : [],
+    TranslateModule.forRoot(),
+    NgbModule,
+    CoreModule,
+    SharedModule,
+    ShellModule,
+    HomeModule,
+    AboutModule,
+    LoginModule,
+    AppRoutingModule // must be imported as the last module as it contains the fallback route
   ],
-  /**
-   * Expose our Services and Providers into Angular's dependency injection.
-   */
+  declarations: [AppComponent],
   providers: [
-    environment.ENV_PROVIDERS,
-    APP_PROVIDERS
-  ]
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,75 +1,35 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import {
-  inject,
-  async,
-  TestBed,
-  ComponentFixture,
-  getTestBed
-} from '@angular/core/testing';
-import { Component } from '@angular/core';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-/**
- * Load the implementations that should be tested.
- */
-import { AppState } from '../app.service';
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
 import { HomeComponent } from './home.component';
-import { Title } from './title';
+import { QuoteService } from './quote.service';
 
-describe(`Home`, () => {
-  let comp: HomeComponent;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-  let injector: TestBed;
-  let service: AppState;
-  let httpMock: HttpTestingController;
 
-  /**
-   * async beforeEach.
-   */
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-      imports: [HttpClientTestingModule],
-      providers: [AppState, Title]
-    })
-
-      /**
-       * Compile template and css.
-       */
+        imports: [
+          CoreModule,
+          SharedModule,
+          HttpClientTestingModule
+        ],
+        declarations: [HomeComponent],
+        providers: [QuoteService]
+      })
       .compileComponents();
-    injector = getTestBed();
-    service = injector.get(AppState);
-    httpMock = injector.get(HttpTestingController);
   }));
 
-  /**
-   * Synchronous beforeEach.
-   */
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
-    comp = fixture.componentInstance;
-
-    /**
-     * Trigger initial data binding.
-     */
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should have default data', () => {
-    expect(comp.localState).toEqual({ value: '' });
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
-
-  it('should have a title', () => {
-    expect(!!comp.title).toEqual(true);
-  });
-
-  it('should log ngOnInit', () => {
-    spyOn(console, 'log');
-    expect(console.log).not.toHaveBeenCalled();
-
-    comp.ngOnInit();
-    expect(console.log).toHaveBeenCalled();
-  });
-
 });
